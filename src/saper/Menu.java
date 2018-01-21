ba2dc31dbd082a56e80347bc0d2ebb29ffa16606
Rafał Dzwonek <34712373+RafalDzwonek@users.noreply.github.com>
2018-01-21 14:56:05 +0100

    Zapętlenie menu i podmenu. Naprawa błędów

--- src/saper/Menu.java
+++ src/saper/Menu.java
@@ -12,66 +12,94 @@ import java.util.Scanner;
  * @author Konrad
  */
 public class Menu {
-        /**
-        * @param n szerokość pola
-        * @param m wysokość pola
-        * @param isDiffSet sprawdza czy poziom trudności został wybrany.
-        */
-        int n;
-        int m;
-        boolean isDiffSet=false;
+
+    /**
+     * @param n szerokość pola
+     * @param m wysokość pola
+     * @param isDiffSet sprawdza czy poziom trudności został wybrany.
+     */
+    int n;
+    int m;
+    boolean isDiffSet = false;
+
     /**
      * Metoda odpowiadająca za Menu gry.
      */
-    public Menu() {
+    public void startMenu() {
         int menu;
+        boolean exit = true;
         System.out.println("Welcome!!!");
-        System.out.println("1. Start \n2. Difficulty \n3. Help \n4. Exit");
-        Scanner read = new Scanner(System.in);
-        menu = read.nextInt();
 
-        switch (menu){
-            case '1': Start();
-                break;
-            case '2': Difficulty();
-                break;
-            case '3': Help();
-                break;
-            case '4': break;
-        }
-        
-        
+        do {
+            System.out.println("1. Start \n2. Difficulty \n3. Help \n4. Exit");
+            Scanner read = new Scanner(System.in);
+            menu = read.nextInt();
 
-        }
+            switch (menu) {
+                case 1:
+                    Start();
+                    break;
+                case 2:
+                    Difficulty();
+                    break;
+                case 3:
+                    Help();
+                    break;
+                case 4:
+                    exit = false;
+                    break;
+                default:
+                    System.out.println("Wrong number.");
+                    break;
+            }
+        } while (exit);
+
+    }
+
     public void Start() {
-    
-}
+
+    }
+
     public void Help() {
-            System.out.println("Help:");
-            System.out.println("");
-}
+        System.out.println("Help:");
+        System.out.println("");
+    }
+
     public void Difficulty() {
-        String difficulty;
-        Scanner read = new Scanner(System.in);
-        difficulty = read.next();
+        int diff;
         Map Mapa;
-        switch (difficulty){
-            case "easy": Mapa = new Map(8,8);
+
+        do {
+            System.out.println("Choose difficulty:\n1.Easy\n2.Normal\n3.Hard\n4.Coustom");
+            Scanner read = new Scanner(System.in);
+            diff = read.nextInt();
+            switch (diff) {
+                case 1:
+                    Mapa = new Map(8, 8);
+                    isDiffSet = true;
                     break;
-            case "normal": Mapa = new Map(16,16);
+                case 2:
+                    Mapa = new Map(16, 16);
+                    isDiffSet = true;
                     break;
-            case "hard": Mapa = new Map(30,16);
+                case 3:
+                    Mapa = new Map(30, 16);
+                    isDiffSet = true;
                     break;
-            case "custom": 
+                case 4:
                     System.out.println("Type width of the map: ");
                     n = read.nextInt();
                     System.out.println("Type height of the map: ");
                     m = read.nextInt();
-                    Mapa = new Map(n,m);
+                    Mapa = new Map(n, m);
+                    isDiffSet = true;
                     break;
+
+                default:
+                    System.out.println("Wrong number.");
+                    break;
+            }
+        } while (!isDiffSet);
     }
-        isDiffSet=true;
-}
-    
- 
+
 }
